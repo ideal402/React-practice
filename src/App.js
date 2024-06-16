b422477112cfa061510d859b27aac85311d2808c
been <ideal402@naver.com>
2024-06-16 15:50:22 +0900

    add Project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './App.css';
+// import Box from "./component/box"
+
+function App() {
+  let counter = 0;
+  const [counter2, setCounter2] = useState(0);
+  
+  const increase = () =>{
+    counter += 1;
+    setCounter2(counter2+1);
+  }; 
+  return (
+    <div>
+      <div>{counter}</div>
+      <div>state: {counter2}</div>
+      <button onClick={increase}>클릭</button>
+    </div>
+  );
+}
+
+export default App;
